cf5d858407b3cc0949dfeec355004d49c130feff
flocaron <100375376+flocaron@users.noreply.github.com>
2024-12-14 00:34:11 +0100

    Add students name

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from Controller.Partie import Partie
 
 if __name__ == "__main__":
+    print("Decruyenaere Hugo - Caron Florimond") 
     noms_joueurs = ["Alice", "Bob", "Charlie"]
     partie = Partie(noms_joueurs)
     partie.jouer(nombre_manches=5)
